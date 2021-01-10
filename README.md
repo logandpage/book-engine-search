6ffa24ac42b1ac097181510165878860a02887a6
Logan <logandpage5@gmail.com>
2021-01-09 19:33:35 -0700

    README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# book-engine-search
+# Book Search Engine
+This application allows you to search any book. You are provided with a picture of the book as well as a short summary of the book. You also have to ability to log in and create an account. With this you can save books that you like
+
+## Installation
+-Javascript
+-moment
+-express
+-mongoose
+-Apollo
+-GraphQL
+
+## Photo(screen-shot)
+![photo](asset/Screenshot.jpg)
+
+## Author
+Logan Page (logandpage)
+
+## Link
+- https://rocky-everglades-54421.herokuapp.com/
+- https://github.com/logandpage/book-engine-search
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { loginUser } from '../utils/API';
+import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 import { LOGIN_USER } from '../utils/mutations';
@@ -12,6 +12,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -31,12 +32,12 @@ const LoginForm = () => {
     try {
       const response = await login({ variables: {...userFormData }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
+      // const { token, user } = await response.json();
+      // console.log(user);
       Auth.login(response.data.login.token);
     } catch (err) {
       console.error(err);
